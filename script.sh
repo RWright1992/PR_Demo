fc9e8d6300c0ebe6d142c3cfe072ba1721886ee3
RWright1992 <ryanwright1992@gmail.com>
2022-09-12 14:35:50 +0000

    adding script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "I am being run from a script"
+touch file1a.txt file2a.txt file3.txt
+echo "Created file1.txt, file2.txt & file3.txt"
+echo "Listing current directory contents"
+ls
